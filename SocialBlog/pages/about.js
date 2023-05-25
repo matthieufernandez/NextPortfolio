203b60d57af0ae86a61578bf7f3bc8949b438dc7
matt <mrmatthieufernandez@gmail.com>
2023-05-25 18:56:55 -0400

    changed name of project

---
+++ SocialBlog/pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <h2>This is a news site</h2>
+      <p>
+        I will be building a site that aggregates news. This is not difficult to
+        do.
+      </p>
+    </>
+  );
+}
